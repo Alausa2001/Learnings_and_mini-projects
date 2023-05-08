dea39a9738473dc5ad1ff398e52f95eafcdab3d2
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-05-08 19:07:09 +0100

    varaibles with staic qualifiers vs those withput static qualifiers

---
+++ C/first_statements/static.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* 
+ * Difference between a variable declared with
+ * a static qualifier and one which is not
+ *
+ * static variables lifetime is throughout the program runtime
+ */
+
+int static_calc() 
+{
+    static int num = 0;
+    num++;
+    return (num);
+}
+
+int calc()
+{
+    int num = 0;
+    num++;
+    return (num);
+}
+
+void main(void)
+{
+    /* output = 3, 2, 1 */
+    printf("%d, %d, %d\n", static_calc(), static_calc(), static_calc());
+
+    /* output = 1, 1, 1 */
+    printf("%d, %d, %d\n", calc(), calc(), calc());
+}
