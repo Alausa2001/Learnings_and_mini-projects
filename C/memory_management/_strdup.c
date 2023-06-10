2a0ca57679e171cc6ee848fd2943fb2c5b25edd0
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-06-10 09:43:11 +0100

    custom strdup

---
+++ C/memory_management/_strdup.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int _strlen(char *str)
+{
+    int i = 0;
+    
+    for(; str[i] != '\0'; i++);
+    return i;
+}
+
+char *_strdup(char *str)
+{
+    int size = _strlen(str); 
+    char *dup;
+    int i;
+
+    dup = (char*)malloc(sizeof(char) * size);
+    for(i = 0; i < size; i++)
+    {
+        dup[i] = str[i];
+    }
+    dup[size] = '\0';
+    return dup;
+}
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s;
+
+    s = _strdup("ALX SE");
+    if (s == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
+    printf("%s\n", s);
+    free(s);
+    return (0);
+}
