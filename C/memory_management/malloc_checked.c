85bbc48a7ff0d706dd5776d9cf62b301e531a965
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-06-12 14:19:40 +0100

    how to exit a function

---
+++ C/memory_management/malloc_checked.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * @brief - function that allocates memory using malloc
+ * 
+ * @param b 
+ * @return void* 
+ */
+
+void *malloc_checked(unsigned int b)
+{
+    char *ptr;
+    
+    ptr = malloc(b);
+
+    if (ptr == NULL)
+      exit(98);
+    return ptr;
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *c;
+    int *i;
+    float *f;
+    double *d;
+
+    c = malloc_checked(sizeof(char) * 1024);
+    printf("%p\n", (void *)c);
+    i = malloc_checked(sizeof(int) * 402);
+    printf("%p\n", (void *)i);
+    f = malloc_checked(sizeof(float) * 100000000);
+    printf("%p\n", (void *)f);
+    d = malloc_checked(INT_MAX);
+    printf("%p\n", (void *)d);
+    free(c);
+    free(i);
+    free(f);
+    free(d);
+    return (0);
+}
